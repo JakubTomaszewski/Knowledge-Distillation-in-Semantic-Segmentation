10e6fdb6ebef6fb6dbc231b6baf3a9f17bf938bd
JakubTomaszewski <jakub.k.tomaszewski@gmail.com>
2022-08-09 23:24:04 +0200

    Add more data transformations to the pipeline

--- src/data_processing/data_transformations.py
+++ src/data_processing/data_transformations.py
@@ -1,26 +1,59 @@
-import numpy as np
 from typing import Tuple
+import numpy as np
 import torch
-from torchvision.transforms import Grayscale, RandomRotation, RandomCrop, Pad, Resize, RandomPerspective
 from torchvision.transforms import Compose
+from torchvision.transforms import (
+                                    Grayscale,
+                                    RandomRotation,
+                                    CenterCrop,
+                                    Pad,
+                                    Resize,
+                                    RandomPerspective,
+                                    RandomAdjustSharpness,
+                                    RandomAutocontrast
+                                    )
+
 
 
 # TODO: Create argParser for transformation params
 
 
-def create_data_transformation_pipeline(img_shape: Tuple, rotation_angle, padding, distortion_factor, fill_value=0) -> Compose:
-    return Compose([Resize(img_shape),
-                    Grayscale(1),
-                    RandomRotation(rotation_angle, fill=fill_value),
-                    Pad(padding, fill=fill_value),
-                    RandomPerspective(distortion_factor, p=1, fill=fill_value)
-                    # RandomCrop(0.9)
-                    ])
+def create_data_transformation_pipeline(img_shape: Tuple,
+                                        rotation_angle: int,
+                                        padding: int,
+                                        distortion_factor: float,
+                                        crop_factor: float,
+                                        sharpness_factor: float,
+                                        fill_value: int=0
+                                        ) -> Compose:
+    return Compose(
+        [
+            Resize(img_shape),
+            Grayscale(1),
+            CenterCrop((np.array(img_shape) * crop_factor).astype(int)),
+            RandomRotation(rotation_angle, fill=fill_value),
+            RandomPerspective(distortion_factor, p=1, fill=fill_value),
+            RandomAdjustSharpness(sharpness_factor, p=1),
+            RandomAutocontrast(p=1),
+            Pad(padding, fill=fill_value)
+        ])
 
 
-def create_label_transformation_pipeline(img_shape: Tuple, rotation_angle, padding, distortion_factor, fill_value=0) -> Compose:
-    return Compose([Resize(img_shape),
-                    RandomRotation(rotation_angle, fill=0),
-                    Pad(padding, fill=fill_value),
-                    RandomPerspective(distortion_factor, p=1, fill=fill_value)
-                    ])
+def create_label_transformation_pipeline(img_shape: Tuple,
+                                         rotation_angle: int,
+                                         padding: int,
+                                         distortion_factor: float,
+                                         crop_factor: float,
+                                         sharpness_factor: float,
+                                         fill_value: int=0
+                                         ) -> Compose:
+    return Compose(
+        [
+            Resize(img_shape),
+            CenterCrop((np.array(img_shape) * crop_factor).astype(int)),
+            RandomRotation(rotation_angle, fill=fill_value),
+            RandomPerspective(distortion_factor, p=1, fill=fill_value),
+            RandomAdjustSharpness(sharpness_factor, p=1),
+            RandomAutocontrast(p=1),
+            Pad(padding, fill=fill_value)
+        ])
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-from pip import main
 from pathlib import Path
 import torch
 from torch.utils.data import DataLoader
+import numpy as np
 import random
 from torchvision.transforms import RandomRotation
 
@@ -9,16 +9,22 @@ from torchvision.transforms import RandomRotation
 # torch.manual_seed(10)
 
 from data_processing.mapillary_dataset import MapillaryDataset
-from data_processing.data_transformations import create_data_transformation_pipeline, create_label_transformation_pipeline
+from data_processing.data_transformations import (
+                                                 create_data_transformation_pipeline,
+                                                 create_label_transformation_pipeline
+                                                 )
 
 
 IMG_SIZE = (400, 600)
-_MAX_ROTATION_ANGLE = 15
-_PADDING=25
-_DISTORTION_FACTOR=0.2
+_MAX_ROTATION_ANGLE = 10
+_PADDING=20
+_DISTORTION_FACTOR=0.1
+_CROP_FACTOR=0.9
+_SHARPNESS_FACTOR=0.5
 
 
 def get_device():
+    device = None
     if torch.cuda.is_available():
         device = torch.device('cuda')
     elif torch.backends.mps.is_available():
@@ -34,21 +40,20 @@ if __name__ == '__main__':
 
     device = get_device()
     print(f'Available device: {device}')
+    print(np.array(IMG_SIZE) * 0.9)
 
-    data_transformation_pipeline = create_data_transformation_pipeline(IMG_SIZE, _MAX_ROTATION_ANGLE, _PADDING, _DISTORTION_FACTOR)
-    label_transformation_pipeline = create_label_transformation_pipeline(IMG_SIZE, _MAX_ROTATION_ANGLE, _PADDING, _DISTORTION_FACTOR)
+    data_transformation_pipeline = create_data_transformation_pipeline(IMG_SIZE, _MAX_ROTATION_ANGLE, _PADDING, _DISTORTION_FACTOR, _CROP_FACTOR, _SHARPNESS_FACTOR, fill_value=0)
+    label_transformation_pipeline = create_label_transformation_pipeline(IMG_SIZE, _MAX_ROTATION_ANGLE, _PADDING, _DISTORTION_FACTOR, _CROP_FACTOR, _SHARPNESS_FACTOR, fill_value=0)
 
     m_dataset = MapillaryDataset(data_path,
                                  labels_path,
                                  sample_transformation=data_transformation_pipeline,
                                  label_transformation=label_transformation_pipeline)
-    
+
     m_dataloader = DataLoader(m_dataset, batch_size=1, shuffle=True)
 
     img, label = m_dataset[0]
-    
-    print(type(img))
-    print(img.shape)
+
     m_dataset.display_image(img)
     m_dataset.display_image(label)
 
