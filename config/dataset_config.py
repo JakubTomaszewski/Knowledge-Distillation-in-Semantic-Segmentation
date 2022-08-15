b36b2bd619dace28af404fef0b36958594de85ad
JakubTomaszewski <jakub.k.tomaszewski@gmail.com>
2022-08-15 21:15:23 +0200

    Feat: Add Mapillary Dataset config
    Added argparse config for data transformations of Mapillary Dataset

---
+++ config/dataset_config.py
@@ -0,0 +1,20 @@
+import argparse
+import numpy as np
+
+
+def parse_dataset_config() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description='U-Net dataset config parser')
+
+    # General
+
+    # Transformations
+    data_transformations = parser.add_argument_group()
+    data_transformations.add_argument('--seed', type=int, default=np.random.randint(2147483647), help='Randomness seed for data transformations')
+    data_transformations.add_argument('--max_rotation_angle', type=int, default=10, help='Max rotation angle')
+    data_transformations.add_argument('--padding', type=int, default=20, help='Padding pixels')
+    data_transformations.add_argument('--distortion_factor', type=float, default=0.1, help='Perspective transform factor')
+    data_transformations.add_argument('--crop_factor', type=float, default=0.9, help='Image center crop factor')
+    data_transformations.add_argument('--sharpness_factor', type=float, default=0.5, help='Default image sharpness factor')
+    data_transformations.add_argument('--fill_value', type=int, default=0, help='Value with which to fill the blank pixels after transformations')
+    
+    return parser.parse_args()
--- src/main.py
+++ src/main.py
@@ -1,22 +1,22 @@
+import sys
 from pathlib import Path
 import torch
 from torch.utils.data import DataLoader
 import numpy as np
 
-from utils.helpers import set_randomness_seed
 from data_processing.mapillary_dataset import MapillaryDataset
 from data_processing.data_transformations import (
                                                  create_data_transformation_pipeline,
                                                  create_label_transformation_pipeline
                                                  )
 
+sys.path.append('..')
+
+from utils.helpers import set_randomness_seed
+from config.dataset_config import parse_dataset_config
+
 
 IMG_SIZE = (400, 600)
-_MAX_ROTATION_ANGLE = 10
-_PADDING=20
-_DISTORTION_FACTOR=0.1
-_CROP_FACTOR=0.9
-_SHARPNESS_FACTOR=0.5
 
 
 def get_device():
@@ -36,23 +36,19 @@ def get_device():
 
 
 if __name__ == '__main__':
-    seed = np.random.randint(2147483647)
+    dataset_config = parse_dataset_config()
 
     data_path = Path('../data/Mapillary_vistas_dataset/training/images')  # load from ArgParse
     labels_path = Path('../data/Mapillary_vistas_dataset/training/labels')
 
     device = get_device()
     print(f'Available device: {device}')
-    print(np.array(IMG_SIZE) * 0.9)
 
-    set_randomness_seed(seed)
-    data_transformation_pipeline = create_data_transformation_pipeline(IMG_SIZE, _MAX_ROTATION_ANGLE, _PADDING, _DISTORTION_FACTOR, _CROP_FACTOR, _SHARPNESS_FACTOR, fill_value=0)
+    set_randomness_seed(dataset_config.seed)
+    data_transformation_pipeline = create_data_transformation_pipeline(IMG_SIZE, dataset_config)
 
-    set_randomness_seed(seed)
-    label_transformation_pipeline = create_label_transformation_pipeline(IMG_SIZE, _MAX_ROTATION_ANGLE, _PADDING, _DISTORTION_FACTOR, _CROP_FACTOR, _SHARPNESS_FACTOR, fill_value=0)
-
-    print('data_transformation_pipeline:', data_transformation_pipeline)
-    print('label_transformation_pipeline:', label_transformation_pipeline)
+    set_randomness_seed(dataset_config.seed)
+    label_transformation_pipeline = create_label_transformation_pipeline(IMG_SIZE, dataset_config)
 
     m_dataset = MapillaryDataset(data_path,
                                  labels_path,
