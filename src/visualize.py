b5f4df5b05df46416a33078d309fe1d83aae37c8
JakubTomaszewski <jakub.k.tomaszewski@gmail.com>
2023-03-21 01:11:27 +0100

    Refactor: Change the main script to data visualization script

--- src/visualize.py
+++ src/visualize.py
@@ -1,8 +1,6 @@
-import sys
+import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
-
-from utils.metrics import Evaluator
-from utils.helpers import set_randomness_seed, display_dict
+from utils.helpers import set_randomness_seed, torch_image_to_numpy
 from config.configs import parse_train_config
 from data_processing.mapillary_dataset import MapillaryDataset
 from data_processing.pipelines.transformation_pipelines import (
@@ -18,6 +16,9 @@ from data_processing.pipelines.processing_pipelines import (
 if __name__ == '__main__':
     # Config
     train_config = parse_train_config()
+    img_shape = (train_config.img_height, train_config.img_width)
+    train_config.crop_size = img_shape
+    
 
     set_randomness_seed(train_config.seed)
 
@@ -27,42 +28,30 @@ if __name__ == '__main__':
     
     # Data processing
     data_preprocessing_pipeline = create_data_preprocessing_pipeline(train_config)
-    prediction_postprocessing_pipeline = create_prediction_postprocessing_pipeline()
+    prediction_postprocessing_pipeline = create_prediction_postprocessing_pipeline(img_shape)
     
     # Dataset
     m_dataset = MapillaryDataset(train_config.train_data_path,
                                train_config.train_labels_path,
                                sample_transformation=data_transformation_pipeline,
                                label_transformation=label_transformation_pipeline,
-                               data_preprocessor=data_preprocessing_pipeline,
+                            #    data_preprocessor=data_preprocessing_pipeline,
                                json_class_names_file_path=train_config.json_class_names_file_path)
 
     # Dataloader
-    m_dataloader = DataLoader(m_dataset, batch_size=train_config.batch_size, shuffle=True)
-
-    # # Model
-    # model = SegformerForSemanticSegmentation.from_pretrained(evaluation_config.model_checkpoint)
-    # model.to(evaluation_config.device)
-
-    # Metrics
-    evaluator = Evaluator(class_labels=m_dataset.id2name.keys(),
-                          ignore_classes=[train_config.void_class_id])
+    m_dataloader = DataLoader(m_dataset, batch_size=1, shuffle=True)
 
     for batch_num, batch in enumerate(m_dataloader):
-        img, label = batch
-        img = img.to(train_config.device)
-        label = label.to(train_config.device)
+        img, label = batch['pixel_values'], batch['labels']
+        img = torch_image_to_numpy(img.squeeze())
 
-        evaluator.update_state(label, label)
+        # Display img and label
+        fig, ax = plt.subplots(2, figsize=(8, 8))
+        ax[0].imshow(img.squeeze())
+        ax[0].set_title('Sample')
+        ax[1].imshow(label.squeeze())
+        ax[1].set_title('Label')
+        plt.show()
 
-        # m_dataset.display_torch_image(img[0].cpu())
-        # m_dataset.display_torch_image(label[0])
-        # masked_label = m_dataset.apply_color_mask(label[0].cpu().squeeze().numpy())
-        # m_dataset.display_numpy_image(masked_label)
-        
-        if batch_num >= 2:
+        if batch_num >= 1:
             break
-    
-    print(evaluator.mean_iou())
-    print(evaluator.mean_iou_per_class())
-    
