e551421823b1031b4fe2bf71f12497004b1c64d3
JakubTomaszewski <jakub.k.tomaszewski@gmail.com>
2023-03-21 01:09:10 +0100

    Refactor: Add `img_shape` param and adapt the pipeline to HuggingFace transformers library

--- src/data_processing/pipelines/processing_pipelines.py
+++ src/data_processing/pipelines/processing_pipelines.py
@@ -1,6 +1,7 @@
+import torch
 import torch.nn as nn
 from argparse import ArgumentParser
-from typing import Callable
+from typing import Callable, Tuple
 from transformers import SegformerImageProcessor
 
 
@@ -18,18 +19,30 @@ def create_data_preprocessing_pipeline(config: ArgumentParser) -> Callable:
     return data_preprocessor
 
 
-def create_prediction_postprocessing_pipeline() -> Callable:
+def create_prediction_postprocessing_pipeline(img_shape: Tuple[int, int]) -> Callable:
     """Factory function which creates a prediction postprocessing pipeline
     used for extracting argmax values and resizing the prediction to its original shape.
+
+    Args:
+        img_shape (Tuple[int, int]): tuple containing the desired img_shape (height, width)
 
     Returns:
         Callable: prediction postprocessing pipeline
     """
-    def prediction_postprocessing_pipeline(output_mask, img_shape):
-        pred_mask_resized = nn.functional.interpolate(output_mask,
+    def prediction_postprocessing_pipeline(prediction: torch.Tensor, _):
+        """Scales the output prediction to the desired size and extracts model's class predictions with the highiest confidence.
+
+        Args:
+            prediction (torch.Tensor): model predictions
+            _ (torch.Tensor): ground truth labels. The param is not used, it is required to match the HuggingFace transformers library
+
+        Returns:
+            torch.Tensor: prediction after postprocessing
+        """
+        prediction_resized = nn.functional.interpolate(prediction,
                             size=img_shape, # (height, width)
                             mode='bilinear',
                             align_corners=False)
-        y_pred = pred_mask_resized.argmax(dim=1)
+        y_pred = prediction_resized.argmax(dim=1)
         return y_pred
     return prediction_postprocessing_pipeline
