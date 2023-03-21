9a13bfca74e0028cdc4da99226178606125f1819
JakubTomaszewski <jakub.k.tomaszewski@gmail.com>
2023-03-22 00:14:03 +0100

    Refactor: Adapt `Evaluator` to work with HuggingFace 🤗 transformers API
    1. Changed argument dtypes from `torch.Tensor` to `np.ndarray` to match the HuggingFace 🤗 transformers API
    2. Created `compute_metrics` function which works with the `transformers.Trainer` class

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -1,4 +1,5 @@
-import torch
+import numpy as np
+from transformers import EvalPrediction
 from collections import defaultdict
 from typing import List
 
@@ -35,14 +36,14 @@ class Evaluator:
         """
         return self.mean_iou()
 
-    def update_state(self, predictions: List[torch.Tensor], labels: List[torch.Tensor]) -> None:
+    def update_state(self, predictions: List[np.ndarray], labels: List[np.ndarray]) -> None:
         """Updates the Evaluator class internal state by calculating the Intersection Over Union (IoU) metric for each class
         and adding it to the total_iou_per_class dict for the corresponding class.
         The updates are performed based on the predictions and labels supplied.
         
         Args:
-            predictions (torch.Tensor): list of all predictions
-            labels (torch.Tensor): list of all ground truth labels
+            predictions (np.ndarray): list of all predictions
+            labels (np.ndarray): list of all ground truth labels
 
         Raises:
             ValueError: when the provided predictions and labels lengths do not match
@@ -51,7 +52,7 @@ class Evaluator:
             raise ValueError('predictions and labels must have the same shape')
 
         for pred, label in zip(predictions, labels):
-            iou_scores = self.iou_score_per_class(pred, label, self.class_labels)
+            iou_scores = self.iou_score_per_class(pred, label)
             for class_id, iou_value in iou_scores.items():
                 if iou_value is not None:
                     self.class_appearances[class_id] += 1
@@ -66,12 +67,12 @@ class Evaluator:
         self.class_appearances = defaultdict(int)
 
     def mean_iou(self) -> float:
-        """Calculates the overall mean IoU value accross all classes.
+        """Calculates the overall mean IoU value accross all classes, based on the internal state (can be used in a sequential loop).
 
         Returns:
             float: mean IoU value accross all classes
         """
-        ious_per_class = self.mean_iou_per_class()
+        ious_per_class = self.iou_per_class()
 
         if len(ious_per_class) == 0:
             print('No samples evaluated')
@@ -79,8 +80,22 @@ class Evaluator:
         else:
             return round(sum(ious_per_class.values()) / len(ious_per_class), 4)
 
-    def mean_iou_per_class(self) -> dict:
-        """Calculates the mean IoU value for each class.
+    def mean_iou_score(self, y_pred: np.ndarray, y_true: np.ndarray) -> float:
+        """Calculates the overall mean IoU value accross all classes for given samples (cannot be used in a sequential loop).
+
+        Returns:
+            float: mean IoU value accross all classes
+        """
+        ious_per_class = self.iou_score_per_class(y_pred, y_true)
+
+        if len(ious_per_class) == 0:
+            print('No samples evaluated')
+            return None
+        else:
+            return round(sum(ious_per_class.values()) / len(ious_per_class), 4)
+
+    def iou_per_class(self) -> dict:
+        """Calculates the mean IoU value for each class, based on the internal state (can be used in a sequential loop).
 
         Returns:
             dict: dict with mean IoU values for each class in the following formar { 'class_id': IoU value }
@@ -92,35 +107,33 @@ class Evaluator:
             iou_per_class_dict[class_id] = round(total_iou / num_appearances, 4)
         return iou_per_class_dict
 
-    def iou_score_per_class(self, y_pred: torch.Tensor, y_true: torch.Tensor, class_labels: List) -> dict:
-        """Calculates the IoU value for a single sample, for each class respectively.
+    def iou_score_per_class(self, y_pred: np.ndarray, y_true: np.ndarray) -> dict:
+        """Calculates the IoU value for a single sample, for each class respectively (cannot be used in a sequential loop).
 
         Args:
-            y_pred (torch.Tensor): predicted mask
-            y_true (torch.Tensor): ground truth label
-            class_labels (List): list containing all possible labels
+            y_pred (np.ndarray): predicted mask
+            y_true (np.ndarray): ground truth label
 
         Returns:
             dict: dictionary with class labels as keys and IoUs as values: { 'class_id': iou_score }
         """
         class_iou_scores = {}
-        class_stats = self._get_prediction_stats(y_pred, y_true, class_labels)
+        class_stats = self._get_prediction_stats(y_pred, y_true)
 
         for label, (tp, fp, fn) in class_stats.items():
             class_iou_scores[label] = self._iou_score(tp, fp, fn)
 
         return class_iou_scores
 
-    def _get_prediction_stats(self, y_pred: torch.Tensor, y_true: torch.Tensor, class_labels: List) -> dict:
+    def _get_prediction_stats(self, y_pred: np.ndarray, y_true: np.ndarray) -> dict:
         """Calculates statistics for a single segmentation prediction such as:
         - number of True Positives (TP)
         - number of False Positives (FP)
         - number of False Negatives (FN)
         
         Args:
-            y_pred (torch.Tensor): predicted mask
-            y_true (torch.Tensor): ground truth label
-            class_labels (List): list containing all possible labels
+            y_pred (np.ndarray): predicted mask
+            y_true (np.ndarray): ground truth label
 
         Returns:
             dict: dict containing the class labels and the corresponding statistics in the following format:
@@ -128,15 +141,18 @@ class Evaluator:
         """
         class_stats = {}
 
-        for label in class_labels:
+        for label in self.class_labels:
             if label in self.ignore_classes:
                 continue
             label_mask = y_true == label
             pred_mask = y_pred == label
 
-            tp = torch.sum(torch.bitwise_and(label_mask, pred_mask)).item()
-            fp = torch.sum(torch.bitwise_and(label_mask, pred_mask) ^ label_mask).item()
-            fn = torch.sum(torch.bitwise_and(label_mask, pred_mask) ^ pred_mask).item()
+            tp = np.sum(np.bitwise_and(label_mask, pred_mask))
+            fp = np.sum(np.bitwise_and(label_mask, pred_mask) ^ label_mask)
+            fn = np.sum(np.bitwise_and(label_mask, pred_mask) ^ pred_mask)
+
+            if (tp + fp + fn) == 0: # If class not present and not predicted
+                continue
 
             class_stats[label] = (tp, fp, fn)
         return class_stats
@@ -152,7 +168,24 @@ class Evaluator:
         Returns:
             float: Intersection Over Union metric (Jaccard Index) value
         """
-        if (tp + fp + fn) == 0: # Zero division (class not present)
+        if (tp + fp + fn) == 0: # Zero division (class not present and not predicted)
             return None
         else:
             return tp / (tp + fp + fn)
+
+    def compute_metrics(self, eval_pred: EvalPrediction) -> dict:
+        """Computes the Mean IoU metric and the IoU per class metric for provided predictions and labels.
+        Works with HuggingFace transformers API.
+        
+        Args:
+            eval_pred (EvalPrediction): input predictions and labels
+
+        Returns:
+            dict: dictionary with stored metrics
+        """
+        y_true, y_pred = eval_pred
+        
+        metrics = {}
+        metrics["mean_iou"] = self.mean_iou_score(y_pred, y_true)
+        metrics["iou_per_class"] = self.iou_score_per_class(y_pred, y_true)
+        return metrics
