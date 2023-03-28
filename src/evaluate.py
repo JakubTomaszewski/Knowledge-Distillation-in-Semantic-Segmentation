Fix: Fix incorrect IoU calculation in `metrics.py`
Fixed incorrect IoU calculation in `metrics.py`. The previous implementation was calculating the total IoU for each class, and then dividing by the number of class appearances. This is incorrect.
To fix this issue, a method of calculating the total number of True Positives, False Positives, and False Negatives for each class was implemented. Then, using these values, the IoU for each class is calculated. The final IoU is then calculated by averaging the IoU for each class.

Changed the `ignore_classes` list param to a single `ignore_class` int param in `metrics.py`.

Refactor: Refactoring change in `segformer.py`. Separated the SegFormer model factory function for into two functions:
- `create_segformer_model_for_train` - initializes a SegFormer model for training (with `num_classes` param as the model head)
- `create_segformer_model_for_inference` - initializes a SegFormer model for inference (evaluation and testing)

src/evaluate.py
@@ -4,8 +4,8 @@ from tqdm import tqdm
 from torch.utils.data import DataLoader
 
 from config.configs import parse_evaluation_config
+from utils.helpers import display_dict, plot_class_distribution_with_iou
 from utils.metrics import Evaluator
-from utils.helpers import display_dict
 from data_processing.mapillary_dataset import MapillaryDataset
 from data_processing.pipelines.transformation_pipelines import (
                                                       create_evaluation_data_transformation_pipeline,
@@ -15,19 +15,19 @@ from data_processing.pipelines.processing_pipelines import (
                                                   create_data_preprocessing_pipeline,
                                                   create_prediction_postprocessing_pipeline
                                                   )
-from models.segformer import create_segformer_model
+from models.segformer import create_segformer_model_for_inference
 
-    
+
 if __name__ == '__main__':
     # Config
     evaluation_config = parse_evaluation_config()
-    
+
     img_shape = (evaluation_config.img_height, evaluation_config.img_width)
 
     # Data transformations
     data_transformation_pipeline = create_evaluation_data_transformation_pipeline(evaluation_config)
     label_transformation_pipeline = create_evaluation_label_transformation_pipeline(evaluation_config)
-    
+
     # Data processing
     data_preprocessing_pipeline = create_data_preprocessing_pipeline(evaluation_config)
     prediction_postprocessing_pipeline = create_prediction_postprocessing_pipeline(img_shape)
@@ -35,8 +35,8 @@ if __name__ == '__main__':
     # Dataset
     dataset = MapillaryDataset(evaluation_config.val_data_path,
                                evaluation_config.val_labels_path,
-                               sample_transformation=data_transformation_pipeline,
-                               label_transformation=label_transformation_pipeline,
+                            #    sample_transformation=data_transformation_pipeline,
+                            #    label_transformation=label_transformation_pipeline,
                                data_preprocessor=data_preprocessing_pipeline,
                                json_class_names_file_path=evaluation_config.json_class_names_file_path)
 
@@ -44,13 +44,12 @@ if __name__ == '__main__':
     eval_dataloader = DataLoader(dataset, batch_size=evaluation_config.batch_size)
 
     # Model
-    model = create_segformer_model(evaluation_config, dataset.num_classes)
-    # model = SegformerForSemanticSegmentation.from_pretrained(evaluation_config.model_checkpoint)
+    model = create_segformer_model_for_inference(evaluation_config, dataset.num_classes)
     model.to(evaluation_config.device)
 
     # Metrics
     evaluator = Evaluator(class_labels=dataset.id2name.keys(),
-                          ignore_classes=[evaluation_config.void_class_id])
+                          ignore_index=evaluation_config.void_class_id)
     # iou_score = evaluate.load('mean_iou')
 
     # Evaluation loop
@@ -60,7 +59,7 @@ if __name__ == '__main__':
         label = label.to(evaluation_config.device)
 
         outputs = model(img).logits
-        predictions = prediction_postprocessing_pipeline(outputs, img_shape)
+        predictions = prediction_postprocessing_pipeline(outputs, None)
 
         evaluator.update_state([predictions.numpy()], [label.numpy()])
         # iou_score.add_batch(predictions=predictions, references=label)
@@ -72,14 +71,13 @@ if __name__ == '__main__':
         # ax[1].imshow(label[0].squeeze())
         # ax[1].set_title('Ground truth')
         # plt.show()
-
         if batch_num >= 2:
             break
 
     print('----------------------')
-    print('Mean IoU', evaluator.mean_iou())
+    print('Mean IoU', evaluator.internal_state_mean_iou())
     print('Class IoU:')
-    display_dict(evaluator.iou_per_class())
+    display_dict(evaluator.internal_state_iou_score_per_class())
     print('----------------------')
 
     # print('Hugging face IoU')

src/train.py
@@ -1,9 +1,9 @@
 import os
 import torch
 import torch.nn as nn
-# import evaluate
-import matplotlib.pyplot as plt
+from copy import deepcopy
 from typing import List, Callable
+from argparse import Namespace
 
 import mlflow
 from torch.utils.data import Dataset
@@ -12,7 +12,7 @@ from transformers.training_args import OptimizerNames
 from transformers.integrations import MLflowCallback
 
 from config.configs import parse_train_config
-from models.segformer import create_segformer_model
+from models.segformer import create_segformer_model_for_train
 from utils.metrics import Evaluator
 from utils.helpers import display_dict
 from data_processing.mapillary_dataset import MapillaryDataset
@@ -28,16 +28,16 @@ from data_processing.pipelines.processing_pipelines import (
                                                   )
 
 
-def configure_mlflow_logger(config):
+def configure_mlflow_logger(config: Namespace):
     os.environ["MLFLOW_EXPERIMENT_NAME"]=config.model_checkpoint
     os.environ["MLFLOW_FLATTEN_PARAMS"]="1"
-    
+
     mlflow.create_experiment(config.model_checkpoint, str(config.mlflow_log_dir))
     mlflow.set_tracking_uri(config.mlflow_log_dir)
-    
 
 
-def create_training_args(config):
+
+def create_training_args(config: Namespace):
     return TrainingArguments(
         run_name=config.model_checkpoint,
         output_dir=config.output_dir,
@@ -93,8 +93,8 @@ def create_trainer(model: nn.Module,
 
 if __name__ == '__main__':
     # Config
-    train_config = parse_train_config()
     evaluation_config = parse_train_config()
+    train_config = parse_train_config()
     
     img_shape = (train_config.img_height, train_config.img_width)
 
@@ -126,19 +126,21 @@ if __name__ == '__main__':
 
     # Metrics
     evaluator = Evaluator(class_labels=train_dataset.id2name.keys(),
-                          ignore_classes=[train_config.void_class_id])
-    # iou_score = evaluate.load('mean_iou')
+                          ignore_index=train_config.void_class_id)
+
+    # Model config
+    id2name = deepcopy(train_dataset.id2name)
+    id2name.pop(train_config.void_class_id)
 
     # Model
-    model = create_segformer_model(train_config, train_dataset.num_classes, train_dataset.id2name)
-    
+    model = create_segformer_model_for_train(train_config, train_dataset.num_classes, id2name)
+
     # Logging
     configure_mlflow_logger(train_config)
     train_callbacks = [
         MLflowCallback()
         ]
-    
-    
+
     # Trainer
     training_args = create_training_args(train_config)
     trainer = create_trainer(model,

src/utils/metrics.py
@@ -9,79 +9,59 @@ class Evaluator:
     """
     def __init__(self, 
                  class_labels: List,
-                 ignore_classes: List=[],
+                 ignore_index: int=255,
                  label_to_class_name: dict={}
                  ) -> None:
         """
         Args:
             class_labels (List): list containing all possible labels
-            ignore_classes (List, optional): class labels which should be ignored during evaluation. Defaults to [].
+            ignore_index (int, optional): class label which should be ignored during evaluation. Defaults to 255.
             label_to_class_name (dict, optional): dict mapping labels to their corresponding class names
             
         Initializes:
-            total_iou_per_class (dict): dict with class labels as keys and the total IoU scores as values
-            class_appearances (dict): dict with class labels as keys and number of their appearances as values
+            class_tp (dict): dict containing True Positives for each class
+            class_fp (dict): dict containing False Positives for each class
+            class_fn (dict): dict containing False Negatives for each class
         """
         self.class_labels = class_labels
-        self.ignore_classes = ignore_classes
+        self.ignore_index = ignore_index
         self.label_to_class_name = label_to_class_name
-        self.total_iou_per_class = defaultdict(float)
-        self.class_appearances = defaultdict(int)
 
-    def __call__(self) -> dict:
-        """Calls the mean_iou() function
+        self.class_tp = defaultdict(int)
+        self.class_fp = defaultdict(int)
+        self.class_fn = defaultdict(int)
+
+    def __call__(self, y_pred: List[np.ndarray], y_true: List[np.ndarray]) -> dict:
+        """Calls the mean_iou_score() function.
+
+        Args:
+            y_pred (List[np.ndarray]): _description_
+            y_true (List[np.ndarray]): _description_
 
         Returns:
             float: mean IoU value accross all classes
         """
-        return self.mean_iou()
+        return self.mean_iou_score(y_pred, y_true)
 
-    def update_state(self, predictions: List[np.ndarray], labels: List[np.ndarray]) -> None:
-        """Updates the Evaluator class internal state by calculating the Intersection Over Union (IoU) metric for each class
-        and adding it to the total_iou_per_class dict for the corresponding class.
-        The updates are performed based on the predictions and labels supplied.
+    def compute_metrics(self, eval_pred: EvalPrediction) -> dict:
+        """Computes the Mean IoU metric and the IoU per class metric for provided predictions and labels.
+        Works with HuggingFace transformers API.
         
         Args:
-            predictions (np.ndarray): list of all predictions
-            labels (np.ndarray): list of all ground truth labels
-
-        Raises:
-            ValueError: when the provided predictions and labels lengths do not match
-        """
-        if len(predictions) != len(labels):
-            raise ValueError('predictions and labels must have the same shape')
-
-        for pred, label in zip(predictions, labels):
-            iou_scores = self.iou_score_per_class(pred, label)
-            for class_id, iou_value in iou_scores.items():
-                if iou_value is not None:
-                    self.class_appearances[class_id] += 1
-                    self.total_iou_per_class[class_id] += iou_value
-                else:
-                    continue
-
-    def reset_state(self):
-        """Resets the Evaluator class internal state by reinitializing: total_iou_per_class and class_appearances dicts
-        """
-        self.total_iou_per_class = defaultdict(float)
-        self.class_appearances = defaultdict(int)
-
-    def mean_iou(self) -> float:
-        """Calculates the overall mean IoU value accross all classes, based on the internal state (can be used in a sequential loop).
+            eval_pred (EvalPrediction): input predictions and labels
 
         Returns:
-            float: mean IoU value accross all classes
+            dict: dictionary with stored metrics
         """
-        ious_per_class = self.iou_per_class()
+        y_true, y_pred = eval_pred
 
-        if len(ious_per_class) == 0:
-            print('No samples evaluated')
-            return None
-        else:
-            return round(sum(ious_per_class.values()) / len(ious_per_class), 4)
+        metrics = {}
+        metrics["mean_iou"] = self.mean_iou_score(y_pred, y_true)
+        metrics["iou_per_class"] = self.iou_score_per_class(y_pred, y_true)
+        return metrics
 
-    def mean_iou_score(self, y_pred: np.ndarray, y_true: np.ndarray) -> float:
-        """Calculates the overall mean IoU value accross all classes for given samples (cannot be used in a sequential loop).
+    def mean_iou_score(self, y_pred: List[np.ndarray], y_true: List[np.ndarray]) -> float:
+        """Calculates the overall mean IoU value accross all classes for given samples.
 
         Returns:
             float: mean IoU value accross all classes
@@ -92,70 +72,89 @@ class Evaluator:
             print('No samples evaluated')
             return None
         else:
-            return round(sum(ious_per_class.values()) / len(ious_per_class), 4)
+            class_ious = ious_per_class.values()
+            present_class_ious = list(filter(lambda x: x is not None, class_ious))
+            return round(sum(present_class_ious) / len(present_class_ious), 4)
 
-    def iou_per_class(self) -> dict:
-        """Calculates the mean IoU value for each class, based on the internal state (can be used in a sequential loop).
-
-        Returns:
-            dict: dict with mean IoU values for each class in the following formar { 'class_id': IoU value }
-        """
-        iou_per_class_dict = {}
-        for (class_id, total_iou), (_, num_appearances) in zip(sorted(self.total_iou_per_class.items()), sorted(self.class_appearances.items())):
-            if class_id in self.label_to_class_name.keys():
-                class_id = f'ID: {class_id}, Name: {self.label_to_class_name[class_id]}'
-            iou_per_class_dict[class_id] = round(total_iou / num_appearances, 4)
-        return iou_per_class_dict
-
-    def iou_score_per_class(self, y_pred: np.ndarray, y_true: np.ndarray) -> dict:
-        """Calculates the IoU value for a single sample, for each class respectively (cannot be used in a sequential loop).
+    def iou_score_per_class(self, y_pred: List[np.ndarray], y_true: List[np.ndarray]) -> dict:
+        """Calculates the IoU value for a single sample, for each class respectively.
 
         Args:
-            y_pred (np.ndarray): predicted mask
-            y_true (np.ndarray): ground truth label
+            y_pred (List[np.ndarray]): list of all predictions (each prediction is a 2D array)
+            y_true (List[np.ndarray]): list of all ground truth labels (each label is a 2D array)
 
         Returns:
             dict: dictionary with class labels as keys and IoUs as values: { 'class_id': iou_score }
         """
         class_iou_scores = {}
-        class_stats = self._get_prediction_stats(y_pred, y_true)
+        class_tp, class_fp, class_fn  = self._get_prediction_stats(y_pred, y_true)
 
-        for label, (tp, fp, fn) in class_stats.items():
-            class_iou_scores[label] = self._iou_score(tp, fp, fn)
-
+        for label_id in self.class_labels:
+            tp = class_tp[label_id]
+            fp = class_fp[label_id]
+            fn = class_fn[label_id]
+            class_iou_scores[label_id] = self._iou_score(tp, fp, fn)
         return class_iou_scores
 
-    def _get_prediction_stats(self, y_pred: np.ndarray, y_true: np.ndarray) -> dict:
-        """Calculates statistics for a single segmentation prediction such as:
-        - number of True Positives (TP)
-        - number of False Positives (FP)
-        - number of False Negatives (FN)
+    def update_state(self, y_pred: List[np.ndarray], y_true: List[np.ndarray]) -> None:
+        """Updates the Evaluator class internal state by calculating the Intersection Over Union (IoU) metric for each class
+        and adding it to the total_iou_per_class dict for the corresponding class.
+        The updates are performed based on the predictions and labels supplied.
         
         Args:
-            y_pred (np.ndarray): predicted mask
-            y_true (np.ndarray): ground truth label
+            y_pred (List[np.ndarray]): list of all predictions (each prediction is a 2D array)
+            y_true (List[np.ndarray]): list of all ground truth labels (each label is a 2D array)
+
+        Raises:
+            ValueError: when the provided predictions and labels lengths do not match
+        """
+        if len(y_pred) != len(y_true):
+            raise ValueError('Predictions and labels must have the same shape')
+
+        class_tp, class_fp, class_fn  = self._get_prediction_stats(y_pred, y_true)
+        self._add_dicts(self.class_tp, class_tp)
+        self._add_dicts(self.class_fp, class_fp)
+        self._add_dicts(self.class_fn, class_fn)
+
+    def reset_state(self):
+        """Resets the Evaluator class internal state by reinitializing dicts containing True Positives, False Positives and False Negatives for each class.
+        """
+        self.class_tp = defaultdict(int)
+        self.class_fp = defaultdict(int)
+        self.class_fn = defaultdict(int)
+
+    def internal_state_mean_iou(self) -> float:
+        """Calculates the overall mean IoU value accross all classes, based on the internal state, which is updated using the `update_state` method.
+        This can be used in a DataLoader sequential loop.
 
         Returns:
-            dict: dict containing the class labels and the corresponding statistics in the following format:
-                { 'class_id': (TP, FP, FN) }
+            float: mean IoU value accross all classes
         """
-        class_stats = {}
+        ious_per_class = self.internal_state_iou_score_per_class()
 
-        for label in self.class_labels:
-            if label in self.ignore_classes:
-                continue
-            label_mask = y_true == label
-            pred_mask = y_pred == label
+        if len(ious_per_class) == 0:
+            print('No samples evaluated')
+            return None
+        else:
+            class_ious = ious_per_class.values()
+            present_class_ious = list(filter(lambda x: x is not None, class_ious))
+            return round(sum(present_class_ious) / len(present_class_ious), 4)
 
-            tp = np.sum(np.bitwise_and(label_mask, pred_mask))
-            fp = np.sum(np.bitwise_and(label_mask, pred_mask) ^ label_mask)
-            fn = np.sum(np.bitwise_and(label_mask, pred_mask) ^ pred_mask)
+    def internal_state_iou_score_per_class(self) -> dict:
+        """Calculates the mean IoU value for each class, based on the internal state, which is updated using the `update_state` method.
+        This can be used in a DataLoader sequential loop.
 
-            if (tp + fp + fn) == 0: # If class not present and not predicted
-                continue
-
-            class_stats[label] = (tp, fp, fn)
-        return class_stats
+        Returns:
+            dict: dict with mean IoU values for each class in the following format { 'class_id': IoU value }
+        """
+        class_iou_scores = {}
+        
+        for label_id in self.class_labels:
+            tp = self.class_tp[label_id]
+            fp = self.class_fp[label_id]
+            fn = self.class_fn[label_id]
+            class_iou_scores[label_id] = self._iou_score(tp, fp, fn)
+        return class_iou_scores
 
     def _iou_score(self, tp: int, fp: int, fn: int) -> float:
         """Calculates the Intersection Over Union metric (Jaccard Index).
@@ -173,19 +172,55 @@ class Evaluator:
         else:
             return tp / (tp + fp + fn)
 
-    def compute_metrics(self, eval_pred: EvalPrediction) -> dict:
-        """Computes the Mean IoU metric and the IoU per class metric for provided predictions and labels.
-        Works with HuggingFace transformers API.
+    def _get_prediction_stats(self, y_pred: List[np.ndarray], y_true: List[np.ndarray]) -> dict:
+        """Calculates statistics for a single segmentation prediction such as:
+        - number of True Positives (TP)
+        - number of False Positives (FP)
+        - number of False Negatives (FN)
         
         Args:
-            eval_pred (EvalPrediction): input predictions and labels
+            y_pred (List[np.ndarray]): list of all predictions (each prediction is a 2D array)
+            y_true (List[np.ndarray]): list of all ground truth labels (each label is a 2D array)
 
         Returns:
-            dict: dictionary with stored metrics
+            dict: dict containing the class labels and the corresponding statistics in the following format:
+                { 'class_id': (TP, FP, FN) }
         """
-        y_true, y_pred = eval_pred
+        class_tp = defaultdict(int)
+        class_fp = defaultdict(int)
+        class_fn = defaultdict(int)
+
+        for prediction, label in zip(y_pred, y_true):
+            for label_id in self.class_labels:
+                mask = label != self.ignore_index
+                mask = np.not_equal(label, self.ignore_index)
+                prediction = prediction[mask]
+                label = np.array(label)[mask]
+
+                label_mask = label == label_id
+                pred_mask = prediction == label_id
+
+                tp = np.sum(np.bitwise_and(label_mask, pred_mask))
+                fp = np.sum(np.bitwise_and(label_mask, pred_mask) ^ label_mask)
+                fn = np.sum(np.bitwise_and(label_mask, pred_mask) ^ pred_mask)
+
+                if (tp + fp + fn) == 0: # If class not present and not predicted
+                    continue
+
+                class_tp[label_id] += tp
+                class_fp[label_id] += fp
+                class_fn[label_id] += fn
+        return class_tp, class_fp, class_fn
+
+    def _add_dicts(self, dict1: dict, dict2: dict) -> None:
+        """Adds two dicts together inplace.
         
-        metrics = {}
-        metrics["mean_iou"] = self.mean_iou_score(y_pred, y_true)
-        metrics["iou_per_class"] = self.iou_score_per_class(y_pred, y_true)
-        return metrics
+        Args:
+            dict1 (dict): first dict
+            dict2 (dict): second dict
+        """
+        for key, value in dict2.items():
+            if key in dict1:
+                dict1[key] = dict1[key] + value
+            else:
+                dict1[key] = value